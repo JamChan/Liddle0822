1819684c509ad0b8e96cb17cac19277aabc721dd
Fang Huai An <developer.fang@gmail.com>
2016-08-22 16:40:41 +0800

    changes

--- Droid/MainActivity.cs
+++ Droid/MainActivity.cs
@@ -7,8 +7,7 @@ namespace Liddle.Droid
 	[Activity (Label = "Liddle", MainLauncher = true, Icon = "@mipmap/icon")]
 	public class MainActivity : Activity
 	{
-		int count = 1;
-
+		
 		protected override void OnCreate (Bundle savedInstanceState)
 		{
 			base.OnCreate (savedInstanceState);
@@ -16,11 +15,10 @@ namespace Liddle.Droid
 			// Set our view from the "main" layout resource
 			SetContentView (Resource.Layout.Main);
 
-			// Get our button from the layout resource,
-			// and attach an event to it
-			Button button = FindViewById<Button> (Resource.Id.myButton);
+			// 
 
-			button.Click += delegate { button.Text = string.Format ("{0} clicks!", count++); };
+
+
 		}
 	}
 }
